ef8ee3949bec2fe49eccd7b9e190553a740f3fd2
Ramajeyam <jeyamjeyam6@gmail.com>
2022-03-14 18:42:08 +0530

    changes

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Button} from 'react-native-elements';
 import { ExternalStyle } from './ExternalStyle';
 import { Component2 } from './Component2';
 
 export default function App() {
   const [view, setView]= useState(false);
+  let text1= `Flex Direction : ${view? 'open' : 'closed'}`
 
   return (
    
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <StatusBar style="auto" 
        animated={true}
        backgroundColor="#80ffaa"
@@ -17,22 +19,36 @@ export default function App() {
       <Text style={{padding:10, margin:10, top:15}}>Hello Iam App.js !!</Text>
       <View style={ExternalStyle.container}>
         <Text style={ExternalStyle.text}>This is a External styles</Text>
-        <Button title="Flex Direction" onPress={()=>setView(!view)}/>
+        <Button title={text1} onPress={()=>setView(!view)}/>
         { view ? <Flexdirection/> : null }
        
       </View>
       <Component2/>
-    </View>
+      <Component2/>
+      <Component2/>
+      <Component2/>
+      <Component2/>
+      <Component2/>
+    </ScrollView>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+});
+
 const Flexdirection = () =>{
-  const [direction, setdirection]= useState('row');
-  console.log(direction)
+  const [direction, setdirection]= useState('column');
+  // console.log(direction)
   return(
     <View style={{flex:1, margin:10,}}>
 
-      <View style={{flex:0.4, flexDirection:"row"}}>
+      <View style={{flex:1, flexDirection:"row"}}>
 
         <View style={{flex:1, margin:5}}>
           <Button title="row" onPress={()=>setdirection("row")}/>
@@ -52,7 +68,7 @@ const Flexdirection = () =>{
       </View>
       {/* {console.log(typeof(direction))} */}
       
-      <View  style={{flex:0.5, margin:10, flexDirection: direction }}>
+      <View  style={{width:270, height:200, margin:20, flexDirection: direction,  borderWidth:2,  }}>
 
         <View style={{flex:1, backgroundColor: 'red'}}/>
         <View style={{flex:2, backgroundColor: 'blue'}}/>
@@ -62,11 +78,4 @@ const Flexdirection = () =>{
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-});
+
--- Component2.js
+++ Component2.js
@@ -1,6 +1,9 @@
-import { ScrollView, Text, StyleSheet } from "react-native"
+import { ScrollView, Text, StyleSheet, View, Button } from "react-native"
+import { useState } from 'react';
 
 export const Component2 = () =>{
+    const [view, setView]= useState(false);
+    let text1= `Justify-Content : ${view? 'open' : 'closed'}`
     const Component2Style = StyleSheet.create({
         text:{
             color:"#ff5c33",
@@ -14,10 +17,77 @@ export const Component2 = () =>{
     })
 
     return(
-        <ScrollView style={Component2Style.container}>
+        <View style={Component2Style.container}>
+            
+        <View >
             <Text style={Component2Style.text}>This is Component 2 </Text>
-            <Text style={Component2Style.text}>This is Component 2 </Text>
-        </ScrollView>
+            
+        </View>
+       
+        <View style={{flex:1, backgroundColor:'white'}}>
+            
+            <Button title={text1} onPress={()=>setView(!view)}/>
+            { view ? <Justifycontent1/> : null }
+               
+            </View>
+        </View>
     )
 
 }
+
+const Justifycontent1 =()=>{
+    const [justify, setjustify]= useState('flex-start');
+
+    return(
+        <View style={{flex:1, margin:10,}}>
+            <View style={{flex:1, flexDirection:"row"}}>
+                <View style={{flex:1, margin:5}}>
+                    <Button title="start" onPress={()=>setjustify("flex-start")}/>
+                </View>
+        
+                <View style={{flex:1,  margin:5}}>
+                    <Button title="center" onPress={()=>setjustify("center")}/>
+                </View>
+        
+                <View style={{flex:1,  margin:5}}>
+                    <Button title="end" onPress={()=>setjustify("flex-end")}/>
+                </View>
+        
+                <View style={{flex:1,  margin:5}}>
+                    <Button title="space-between" onPress={()=>setjustify("space-between")}/>
+                </View>
+
+                <View style={{flex:1,  margin:5}}>
+                    <Button title="space-around" onPress={()=>setjustify("space-around")}/>
+                </View>
+
+                <View style={{flex:1,  margin:5}}>
+                    <Button title="space-evenly" onPress={()=>setjustify("space-evenly")}/>
+                </View>
+            </View>
+            {/* {console.log(justify)} */}
+        
+            <View  style={{flex:1, justifyContent: justify, margin: 20, flexDirection: "row",  alignContent:"center", borderWidth:2 }}>
+    
+                <View style={{height:50, width:50, backgroundColor: 'red'}}> 
+                    <Text style={styles.textContainer}>R</Text>
+                </View>
+
+                <View style={{height:50, width:50, backgroundColor: 'blue'}}>
+                    <Text style={styles.textContainer}>B</Text>
+                </View>
+
+                <View style={{height:50, width:50, backgroundColor: 'green'}}>
+                    <Text style={styles.textContainer}>G</Text>
+                </View>
+            </View>
+      </View>
+    )
+}
+
+const styles = {
+    textContainer: {
+      fontSize: 26,
+      fontWeight: "100",
+    },
+  };
